885455989700f1cf233923707385f40bea5b3883
sandy1110 <sandiig.1110@gmail.com>
2022-07-12 19:25:15 -0500

    log out / log in buttons and functionality added to bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { useEffect } from 'react';
-import { Home } from "./components/Home";
+import React, { useEffect, useState } from 'react';
+import { Tabs, Tab, AppBar } from '@material-ui/core';
 import { useMoralis } from "react-moralis";
 import Button from '@mui/material/Button';
 
-
 function App() {
-
+  const [selectedTab, setSelectedTab] = useState(0);
   const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
 
   useEffect(() => {
-      if (isAuthenticated) {
-        // add your logic here
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isAuthenticated]);
+    if (isAuthenticated) {
+      // add your logic here
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated]);
+  
+  const handleSelectedTab = (event, newTabValue) => {
+    setSelectedTab(newTabValue);
+  }
 
   const login = async () => {
     if (!isAuthenticated) {
-
       await authenticate({signingMessage: "Log in using Moralis" })
         .then(function (user) {
           console.log("logged in user:", user);
           console.log(user.get("ethAddress"));
-          alert("Welcome, wallet connected! " + user.get("ethAddress"));
         })
         .catch(function (error) {
           console.log(error);
@@ -37,11 +38,21 @@ function App() {
 
   return (
     <>
-    <div align="right">
-      <Button variant="contained" onClick={login}> Metamask Login </Button>
-      <Button variant="contained" onClick={logOut} disabled={isAuthenticating}> Logout </Button>
-    </div>
-      <Home/>
+      <header>
+        <AppBar position="static">
+          <Tabs value={selectedTab} onChange={handleSelectedTab} centered>
+            {/* Add tabs here to displlay in te page */}
+            <Tab label="About" />
+            <Tab label="Apply" />
+            <Tab label="Invest" />
+            <Tab label="Contact" />
+            {!isAuthenticated &&
+              <Button variant="contained" onClick={login}> Metamask Login </Button>}
+            {isAuthenticated &&
+              <Button variant="contained" onClick={logOut}> Logout </Button>}
+          </Tabs>
+        </AppBar>
+      </header>
     </>
   );
 }
