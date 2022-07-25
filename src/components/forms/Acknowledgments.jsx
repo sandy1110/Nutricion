ad5fc2ace7ecf87efdad19adaaf509f6611acf3f
sandy1110 <sandiig.1110@gmail.com>
2022-07-25 12:49:21 -0500

    name elements in Acknowledgment

--- src/components/forms/Acknowledgments.jsx
+++ src/components/forms/Acknowledgments.jsx
@@ -1,58 +1,15 @@
 import React, { useState } from 'react';
-//import { NumberFormat } from 'react-number-format';
-import { FormControl, FormControlLabel, FormLabel, RadioGroup } from '@mui/material';
-import { Box, Paper, Radio, TextField, Typography } from '@material-ui/core';
+import { Box, Paper,  TextField, Typography } from '@material-ui/core';
 
-const initialValues ={
-    name:'',
-    alternateNames:'',
-    age:'',
-    sss:'',
-    citizenship:'',
-    credit:'',
-    maritalStatus:'',
-    otherBorrowes:'',
-    dependents:'',
-    street:'',
-    unit:'',
-    city:'',
-    state:'',
-    zip:'',
-    country:'',
-
+const initialValues ={  
+    date1: '',
+    date2: '',
+    signature: '',
+    signature2: '',
 }
 
-/*const NumberFormatCustom = React.forwardRef(function NumberFormatCustom(props, ref) {
-    const { onChange, ...other } = props;
-  
-    return (
-      <NumberFormat
-        {...other}
-        getInputRef={ref}
-        onValueChange={(values) => {
-          onChange({
-            target: {
-              name: props.name,
-              value: values.value,
-            },
-          });
-        }}
-        thousandSeparator
-        isNumericString
-        prefix="$"
-      />
-    );
-  });*/
-
 export const Acknowledgments = () => {
-    const [values, setValues] =useState(initialValues);
 
-    const handleInputChange = event =>{
-        const [name, value] = event.target;
-        setValues([...values,
-            [name].value
-        ]);
-    }
     return (
         <Box display="flex" flexDirection="column" gridRowGap={25}>
             <Paper sx={{backgroundColor:"black"}}>
@@ -177,7 +134,7 @@ export const Acknowledgments = () => {
                             fullWidth
                             variant="standard"
                             label="Borrower Signature"
-                            name='name'
+                            name='siganture'
                         />
                         <br></br>
                         <br></br>
@@ -186,7 +143,7 @@ export const Acknowledgments = () => {
                         <TextField  
                             variant="standard"
                             label="Date (mm/dd/yyyy)"
-                            name='name'
+                            name='date1'
                         />
                         
                     </Box>
@@ -197,14 +154,14 @@ export const Acknowledgments = () => {
                             fullWidth
                             variant="standard"
                             label="Addittional Borrower Signature"
-                            name='name'
+                            name='signature2'
                         />
                     </Box>
                     <Box sx={{p:2, margin:2, width:"20%"}}>
                         <TextField  
                             variant="standard"
                             label="Date (mm/dd/yyyy)"
-                            name='name'
+                            name='date2'
                         />  
                     </Box>
                 </Box>
