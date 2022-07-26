0c061a10b57f0c0bb14a656ff0f549be2e6b9462
sandy1110 <sandiig.1110@gmail.com>
2022-07-26 13:15:27 -0500

    Change agreements message

--- src/components/forms/Acknowledgments.jsx
+++ src/components/forms/Acknowledgments.jsx
@@ -169,21 +169,12 @@ export const Acknowledgments = () => {
                                 (g)  other actions permissible under applicable law.
                             </ul>
                         </Typography>              
-                    </Box>        
+                    </Box>
                 </Box>
-                <Box display="flex" flexDirection>
-                    <Box sx={{p:2, margin:2, width:"80%"}}>
-                        <RadioGroup row>
-                            <FormControlLabel value="individual" control={<Radio size='small'/>} label="I have read and I accept the terms and conditions on this document." />
-                        </RadioGroup>
-                    </Box>
-                    <Box sx={{p:2, margin:2, width:"20%"}}>
-                        <TextField  
-                            variant="standard"
-                            label="Date (mm/dd/yyyy)"
-                            name='date'
-                        />
-                    </Box>
+                <Box sx={{p:2, margin:2}}>
+                    <RadioGroup row>
+                        <FormControlLabel value="individual" control={<Radio size='small'/>} label="I hereby accept the Terms and Conditions, Acknoledgements and Agreements." />
+                    </RadioGroup>
                 </Box>
                 {/*<Box display="flex" flexDirection>
                     <Box sx={{p:2, margin:2, width:"80%"}}>
