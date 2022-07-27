f7940b29bd256fb8424199dfc58e32c37caa7ebb
Andres Carreon <andres.carreondiaz@hotmail.com>
2022-07-26 22:46:06 -0500

    Property card updates Map marker when click on icon

--- src/Helpers/CarouselData.js
+++ src/Helpers/CarouselData.js
@@ -9,7 +9,10 @@ export const images = [
         value: 200000,
         area: 120,
         capRate: 7.5,
-        address: "1501 Oak Road, San Diego, California"
+        address: "1501 Oak Road, San Diego, California",
+        propertyLat: 32.803963,
+        propertyLng: -117.130824
+
     },
     {
         img: Orlando,
@@ -17,7 +20,9 @@ export const images = [
         value: 300000,
         area: 150,
         capRate: 8.5,
-        address: "1634 Beach Blvd, Orlando, Florida"
+        address: "1634 Beach Blvd, Orlando, Florida",
+        propertyLat: 28.523188,
+        propertyLng: -81.351497
     },
     {
         img: Austin,
@@ -25,7 +30,9 @@ export const images = [
         value: 400000,
         area: 100,
         capRate: 7.2,
-        address: "854 Mountain Road, Austin, Texas"
+        address: "854 Mountain Road, Austin, Texas",
+        propertyLat: 30.274579,
+        propertyLng: -97.747836
     },
     {
         img: Austin,
@@ -33,6 +40,8 @@ export const images = [
         value: 200000,
         area: 110,
         capRate: 6.4,
-        address: "1415 Green Ave, McAllen, Texas"
+        address: "1415 Green Ave, McAllen, Texas",
+        propertyLat: 30.328174,
+        propertyLng: -97.688982
     }
 ]
--- src/components/CardProperty.jsx
+++ src/components/CardProperty.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Box, Button } from "@mui/material"
 import "./CardProperties.css"
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-export const CardProperty = ({ propertyData }) => {
+export const CardProperty = ({ propertyData, mapLat, mapLng, setMapLat, setMapLng }) => {
 
-    const { img, id, value, area, capRate, address } = propertyData
+    const { img, id, value, area, capRate, address, propertyLat, propertyLng } = propertyData
+
+    const handlePropertyClick = () => {
+        setMapLat(propertyLat)
+        setMapLng(propertyLng)
+    }
 
     return (
         <Box className='box'>
-            <img className="cardImg" src={img} alt="Property card"/>
+            <img className="cardImg" src={img} alt="Property card" />
             <div className='information'>
                 <div className='mortgageId'>Mortgage ID {id}</div>
                 <div className='propertyValue'>Value ${value} USD</div>
@@ -16,7 +22,15 @@ export const CardProperty = ({ propertyData }) => {
                 <div className='capRate'>Cap Rate: {capRate}</div>
                 <div className='address'>Address: {address}</div>
                 <div className='centerButton'>
-                    <Button style={{backgroundColor: "orange",borderRadius: 35}} variant='contained' className='buttonInvest'>Invest</Button>
+                    <Button
+                        style={{ backgroundColor: "orange", borderRadius: 35 }}
+                        variant='contained'
+                        className='buttonInvest'
+                        onClick={handlePropertyClick}
+                    >
+                        Invest
+                    </Button>
+                    <LocationOnIcon className='locationIcon' onClick={handlePropertyClick}/>
                 </div>
             </div>
         </Box>
