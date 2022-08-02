8df47a4e1cef158c711cd41e08f4156e20710ff0
sandy1110 <sandiig.1110@gmail.com>
2022-08-01 20:25:56 -0500

    contact form

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -3,13 +3,27 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import useForm from '../../hooks/useForm'
 import "../Forms.css"
+import { Typography, Paper } from '@mui/material';
 
 
 export const Contact = () => {
 
+    const boxSX = {
+        backgroundColor: "#ff914d",
+        width:"70%",
+        "&:hover": {
+          color: 'white',
+          backgroundColor: '#ffbd59'
+        },
+        justifyContent: 'center',
+        m: 2
+      };
 
-
-    const [form, setForm, handleChange] = useForm({ email: "", })
+    const [form, setForm, handleChange] = useForm({ 
+        subject: "", 
+        email: "",
+        message: "",
+    })
 
     const submit = (e) => {
         e.preventDefault();
@@ -18,7 +32,9 @@ export const Contact = () => {
         const requestOptions = {
             method: 'POST',
             body: JSON.stringify({
-                "email": form.email
+                "subject": form.subject,
+                "email": form.email,
+                "message": form.message
             }),
             headers: {
                 'Content-Type': 'application/json'
@@ -26,7 +42,7 @@ export const Contact = () => {
         }
 
         try{
-            fetch("https://mortgage-defi-api.herokuapp.com/api/v1/email/sendEmail", requestOptions).then( console.log("email sent."));
+            fetch("http://localhost:8080/api/v1/contactEmail/sendContactEmail", requestOptions).then( console.log("email sent."));
 
         }
         catch{
@@ -36,19 +52,34 @@ export const Contact = () => {
 
     return (
         <form>
-            <Box className='Box'>
-                <TextField
-                    id="standard-basic"
-                    label="Email"
-                    variant="standard"
-                    name="email"
-                    value={form.email}
-                    onChange={handleChange}
-                />
+            <Box sx={{display:"flex", flexDirection:"column", minHeight:"85vh", alignItems:"center", justifyContent: "center"}}>
+                <Paper elevation={8}>
+                    <Box sx={{background:"#ffbd59", py:3, px:6, borderRadius:1}}>
+                        <Typography variant="h6" >CONTACT US</Typography>
+                    </Box>
+                    <Box sx={{display:"flex", flexDirection:"column", px:4, py:2, gridRowGap:13, minWidth:"35vw", alignItems:"center", justifyContent: "center"}}>
+                        <TextField size="small" fullWidth
+                            label="Subject"
+                            name="subject"
+                            value={form.subject}
+                            onChange={handleChange}
+                        />
+                        <TextField size="small" fullWidth
+                            label="Email"
+                            name="email"
+                            value={form.email}
+                            onChange={handleChange}
+                        />
+                        <TextField multiline fullWidth rows={6} size="small"
+                            label="Message"
+                            name="message"
+                            value={form.message}
+                            onChange={handleChange}
+                        />
+                        <Button sx={boxSX} onClick={submit} variant="contained" >Submit</Button>
+                    </Box>
+                </Paper>
             </Box>
-            <div className='Button'>
-                <Button onClick={submit} variant="contained" >Submit</Button>
-            </div>
         </form>
     )
 }
