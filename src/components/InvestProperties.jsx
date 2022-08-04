815c426259144fd24e77568ec8d1c14b7b1e385c
sandy1110 <sandiig.1110@gmail.com>
2022-08-04 15:33:06 -0500

    searchBar

--- src/components/InvestProperties.jsx
+++ src/components/InvestProperties.jsx
@@ -4,6 +4,9 @@ import { images } from '../helpers/CarouselData'
 import SearchIcon from '@mui/icons-material/Search';
 import { MapComponent } from "./MapComponent";
 import { useState } from 'react'
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
 
 
 
@@ -11,25 +14,50 @@ export const InvestProperties = () => {
 
     const [mapLat, setMapLat] = useState(32.803963)
     const [mapLng, setMapLng] = useState(-117.130824)
+    const [busqueda, setBusqueda] = useState("");
+
+    const handleChange = (event) => {
+        setBusqueda(event.target.value);
+    };
+  
 
     const datosPropiedades = images.map((property) => {
-        return (
-            <CardProperty
-                key={property.id}
-                propertyData={property}
-                setMapLat={setMapLat}
-                setMapLng={setMapLng}
-            />)
+        if(property.address.toLowerCase().includes(busqueda.toLowerCase())){
+            console.log(property.address)
+            return (
+                <CardProperty
+                    key={property.id}
+                    propertyData={property}
+                    setMapLat={setMapLat}
+                    setMapLng={setMapLng}
+                />
+            )
+        }
     })
-
+        
     return (
         <div className="container">
             <nav>
                 <h2>Where do you want to invest?</h2>
+                <Paper elevation={7}
+                    sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 325, my: 2}}
+                    >
+                    <InputBase
+                        sx={{ ml: 1, flex: 1 }}
+                        value={busqueda}
+                        onChange={handleChange}
+                        placeholder="Search..."
+                        inputProps={{ 'aria-label': 'search google maps' }}
+                    />
+                    <IconButton  sx={{ p: '10px',  color: '#ffa040'}} aria-label="search">
+                        <SearchIcon />
+                    </IconButton>
+                </Paper>
             </nav>
             <main>
                 <MapComponent lat={mapLat} lng={mapLng} />
-            </main>
+            </main>  
+            
             <div className="sidebar">
                 {datosPropiedades}
             </div>
