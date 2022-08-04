ae1640f0361223a5dc344e401dd737f0c0cdfeda
Andres Carreon <andres.carreondiaz@hotmail.com>
2022-08-04 07:31:20 -0500

    cards display information in grid and more information expanded

--- src/Helpers/CarouselData.js
+++ src/Helpers/CarouselData.js
@@ -7,21 +7,22 @@ export const images = [
         img: SanDiego,
         id: 13009,
         value: 200000,
-        area: 120,
+        dimension: 120,
         capRate: 7.5,
-        address: "1501 Oak Road, San Diego, California",
+        address: "1501 Oak Rd, San Diego, California",
         propertyLat: 32.803963,
         propertyLng: -117.130824,
         creditScore: 754,
         downPayment: 10000,
         debtIncomeRatio: 40,
+        description: "Incredible house with 3 rooms, close to the downtown"
 
     },
     {
         img: Orlando,
         id: 32000,
         value: 300000,
-        area: 150,
+        dimension: 150,
         capRate: 8.5,
         address: "1634 Beach Blvd, Orlando, Florida",
         propertyLat: 28.523188,
@@ -29,12 +30,13 @@ export const images = [
         creditScore: 801,
         downPayment: 5000,
         debtIncomeRatio: 20,
+        description: "Big and modern house outside the city with greate view"
     },
     {
         img: Austin,
         id: 45226,
         value: 400000,
-        area: 100,
+        dimension: 100,
         capRate: 7.2,
         address: "854 Mountain Road, Austin, Texas",
         propertyLat: 30.274579,
@@ -42,12 +44,13 @@ export const images = [
         creditScore: 400,
         downPayment: 25000,
         debtIncomeRatio: 50,
+        description: "Big house close to the university with malls around"
     },
     {
         img: Austin,
         id: 4526,
         value: 400000,
-        area: 110,
+        dimension: 110,
         capRate: 6.4,
         address: "1415 Green Ave, McAllen, Texas",
         propertyLat: 30.328174,
@@ -55,5 +58,6 @@ export const images = [
         creditScore: 850,
         downPayment: 30000,
         debtIncomeRatio: 5,
+        description: "Property close to the town mall, located in front of a golf club"
     }
 ]
--- src/components/CardProperty.jsx
+++ src/components/CardProperty.jsx
@@ -20,6 +20,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import HomeIcon from '@mui/icons-material/Home';
 
 
 
@@ -29,7 +30,7 @@ export const CardProperty = ({ propertyData, mapLat, mapLng, setMapLat, setMapLn
         img,
         id,
         value,
-        area,
+        dimension,
         capRate,
         address,
         propertyLat,
@@ -37,6 +38,7 @@ export const CardProperty = ({ propertyData, mapLat, mapLng, setMapLat, setMapLn
         creditScore,
         downPayment,
         debtIncomeRatio,
+        description
     } = propertyData
 
     const handlePropertyClick = () => {
@@ -110,16 +112,10 @@ export const CardProperty = ({ propertyData, mapLat, mapLng, setMapLat, setMapLn
                 <CardHeader
                     avatar={
                         <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                            R
                         </Avatar>
                     }
-                    action={
-                        <IconButton aria-label="settings">
-                            <MoreVertIcon />
-                        </IconButton>
-                    }
-                    title="Shrimp and Chorizo Paella"
-                    subheader="September 14, 2016"
+                    title={address}
+                    subheader={`Mortgage ID: ${id}`}
                 />
                 <CardMedia
                     component="img"
@@ -129,18 +125,28 @@ export const CardProperty = ({ propertyData, mapLat, mapLng, setMapLat, setMapLn
                 />
                 <CardContent>
                     <Typography variant="body2" color="text.secondary">
-                        This impressive paella is a perfect party dish and a fun meal to cook
-                        together with your guests. Add 1 cup of frozen peas along with the mussels,
-                        if you like.
+                        <div className='container-cardProperty'>
+                            <div className='up-left'>Value:<br />${value} USD</div>
+                            <div className='up-right'>Interest Rate: <br /> {capRate}%</div>
+                            <div className='down-left'>Credit score: <br /> {creditScore}</div>
+                            <div className='down-right'>Down payment: <br /> ${downPayment} USD</div>
+                        </div>
                     </Typography>
                 </CardContent>
-                <CardActions disableSpacing>
+                <CardActions disableSpacing className='buttons-actions'>
                     <IconButton aria-label="add to favorites">
                         <FavoriteIcon />
                     </IconButton>
-                    <IconButton aria-label="share">
-                        <ShareIcon />
-                    </IconButton>
+                    <div className='centerButton'>
+                        <Button
+                            style={{ backgroundColor: "orange", borderRadius: 35 }}
+                            variant='contained'
+                            className='buttonInvest'
+                            onClick={callMortgageContractTest}
+                        >
+                            Invest
+                        </Button>
+                    </div>
                     <ExpandMore
                         expand={expanded}
                         onClick={handleExpandClick}
@@ -152,31 +158,16 @@ export const CardProperty = ({ propertyData, mapLat, mapLng, setMapLat, setMapLn
                 </CardActions>
                 <Collapse in={expanded} timeout="auto" unmountOnExit>
                     <CardContent>
-                        <Typography paragraph>Method:</Typography>
                         <Typography paragraph>
-                            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                            aside for 10 minutes.
-                        </Typography>
-                        <Typography paragraph>
-                            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                            large plate and set aside, leaving chicken and chorizo in the pan. Add
-                            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                            stirring often until thickened and fragrant, about 10 minutes. Add
-                            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                        </Typography>
-                        <Typography paragraph>
-                            Add rice and stir very gently to distribute. Top with artichokes and
-                            peppers, and cook without stirring, until most of the liquid is absorbed,
-                            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                            mussels, tucking them down into the rice, and cook again without
-                            stirring, until mussels have opened and rice is just tender, 5 to 7
-                            minutes more. (Discard any mussels that don&apos;t open.)
+                            {description}
                         </Typography>
                         <Typography>
-                            Set aside off of the heat to let rest for 10 minutes, and then serve.
+                            <ul>
+                                <li>Dimension: {dimension} feat<sup>2</sup></li>
+                                <li>Debt Income Ratio: {debtIncomeRatio}%</li>
+                            </ul>
                         </Typography>
+
                     </CardContent>
                 </Collapse>
             </Card>
