8084723dda649df43ab1636c0481effc63bf2c94
Andres Carreon <andres.carreondiaz@hotmail.com>
2022-08-05 19:18:34 -0500

    our story section is displayed

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,20 +1,33 @@
 import "./About.css";
 import CryptoImage from "../../assets/CryptoImage.jpg"
 
+
 export const About = () => {
     return (
         <div className="about-container">
             <div className="header-line">
-                <img className="about-background" src={CryptoImage}/>
+                <img className="about-background" src={CryptoImage} />
                 <h1 className="about-title">
-                    We are criptofor <br/> mortgage
+                    We are criptofor <br /> mortgage
                 </h1>
             </div>
             <div className="story">
-                Our Story
+                <h1 className="story-title">
+                    Our Story
+                </h1>
+                <h2 className="story-text">
+                    How have people survived all this time with a
+                    traditional mortgage system? <br />
+                    We have experienced the difficulties to request a mortgage, also we  <br />
+                    have experienced the pain to invest in Real Estate <br /> <br /> <br />
+                    This is the reason we built a platform where people can invest in Real Estate <br />
+                    and also request a mortgage in the same place using crypto
+                </h2>
             </div>
             <div className="we-do">
-                What we do
+                <h1 className="we-do-title">
+                    What we do
+                </h1>
             </div>
             <div className="testimony">
                 Testimony here
