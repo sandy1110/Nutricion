bc285443802c140831456c2f18bec75af9831f4a
sandy1110 <sandiig.1110@gmail.com>
2022-08-08 15:10:02 -0500

    call api for saving personal information

--- src/components/forms/PersonalInformation.jsx
+++ src/components/forms/PersonalInformation.jsx
@@ -65,7 +65,20 @@ export const PersonalInformation = () => {
 
     const onSubmit = ( event ) => {
         event.preventDefault();
-        console.log(formValues)
+        console.log(formValues);
+        const requestOptions = {
+            method: 'POST',
+            body: JSON.stringify({formValues}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try{
+            fetch(process.env.REACT_APP_MORTGAGE_FIRST_SECTION, requestOptions).then( console.log("information sent."));    
+        }
+        catch{
+            alert("Error");
+        }
     }
     
     const [name, setName] = useState('');
